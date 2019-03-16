ca44dc01d30b5f8f4b87f351417bea22c72cf0ba
Hyeonseong Jeon <cutz309@gmail.com>
2019-03-16 20:18:00 +0900

    Update lec5-1[conv].py

--- lec5-1[conv].py
+++ lec5-1[conv].py
@@ -8,6 +8,13 @@ F = 5 # filter size
 S = 2 # stride 2
 
 v = int((x + P*2 - F) / S + 1) # feature map size
+W0 = np.random.normal(size=(5,5,4))
+b0 = np.array(1)
 
 V = np.zeros(shape=[v, v, 1], dtype=np.float32)
 
+
+V[0, 0, 0] = np.sum(X[:5, :5, :] * W0) + b0
+V[1, 0, 0] = np.sum(X[2:7, :5, :] * W0) + b0
+V[2, 0, 0] = np.sum(X[4:9, :5, :] * W0) + b0
+V[3, 0, 0] = np.sum(X[6:11, :5, :] * W0) + b0
